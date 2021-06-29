a4869c35986c7df12aa8a6120634a0ebf59ce954
oasis10702 <oasis10702@gmail.com>
2021-06-30 00:19:52 +0900

    Add slide navigation

--- js/common.js
+++ js/common.js
@@ -1,17 +1,17 @@
 let mvItems = $('.mv-item');
 let mvUrl = 'url("./img/home/mv_';
-$('.mv-item').each(function (index, elem) {
+$('.mv-item').each(function(index, elem) {
   $(elem).css('background-image', mvUrl + index + '.jpeg")');
 });
 // url("./img/home/mv_1.jpeg")
-$(document).ready(function () {
+$(document).ready(function() {
   $('.mv-box').slick({
     autoplay: true,
     dots: true,
+    dotsClass: 'slick-dots',
     infinite: true,
     speed: 500,
     prevArrow: '<span class="slide-arrow prev-arrow"><<span>',
-    nextArrow: '<span class="slide-arrow next-arrow">><span>',
+    nextArrow: '<span class="slide-arrow next-arrow">><span>'
   });
 });
-console.log($('.mv-item')[0]);
